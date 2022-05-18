801976cd4640f0c952840f6983d21ab7d5b6a307
INFO5Cattedra <INFO5Cattedra@INFO5CATTEDRA>
2022-05-18 10:16:19 +0200

    Aggiungere i file di progetto.

---
+++ Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AS2122_4E_INF_TogniEmanuele_GestioneArticoli
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, Articolo> articoli;
+        public Form1()
+        {
+            InitializeComponent();
+            articoli = new Dictionary<string, Articolo>();
+        }
+    }
+}
